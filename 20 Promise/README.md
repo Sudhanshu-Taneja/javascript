0996acfee4457ccac40ff3e16a147027f0d192d6
Sudhanshu TANEJA <sudhanshutaneja001@gmail.com>
2025-09-17 09:36:25 +0530

    Promise in js

---
+++ 20 Promise/README.md
@@ -0,0 +1,7 @@
+# Promises
+
+The promise object represents the eventual completion or failure of an asynchronous operation and its resulting value. A promise is in one of these states - 
+
+1. Pending - Initial state, neither fulfilled nor rejected.
+2. Fulfilled - Meaning that the operation was completed successfully.
+3. Rejected - Meaning that the operation failed.
---
+++ 20 Promise/promise.js
@@ -0,0 +1,83 @@
+const promiseOne = new Promise(function(resolve, reject){
+    setTimeout(function(){
+        console.log('Async task is complete.');  
+        resolve();
+    },1000)
+
+})
+
+promiseOne.then(function(){
+    console.log("Promise consumed");
+})
+
+new Promise(function (resolve, reject){
+    setTimeout(function(){
+        console.log("Async task 2 is complete");
+        resolve();
+    },1000)
+}).then(function(){
+    console.log("Promise 2 consumed");
+})
+
+const promiseThree = new Promise(function(resolve, reject){
+    setTimeout(function(){
+        resolve({username: 'Sudhanshu', email: 'sudhanshu@example.com'})
+    },1500)
+})
+
+promiseThree.then(function(user){
+    console.log(user);
+})
+
+const promiseFour = new Promise(function(resolve,reject){
+
+    setTimeout(function(){
+        let error = true;
+        if(!error) resolve("Promise 4 resolved");
+        else reject("Promise 4 failed");
+    },2000)
+
+})
+
+promiseFour.then((user)=>{
+    console.log(user);
+    return user.username
+}).then((username)=>{
+    console.log(username);
+})
+.catch((error)=>{
+    console.log(error);
+})
+.finally(()=>{
+    console.log("Promise 4 settled");
+})
+
+const promiseFive = new Promise((resolve, reject)=>{
+    setTimeout(()=>{
+        let error = false;
+        if(!error) resolve("Promise 5 resolved");
+        else reject("Promise 5 failed");
+    },1000);
+});
+
+async function consumePromiseFive(){
+    const res = await promiseFive
+    .then((message)=>{
+        console.log(message);
+    })
+    .catch((error)=>{
+        console.log(error);
+    })
+    .finally(()=>{
+        console.log("Promise 5 settled");
+    });
+
+    console.log(res);
+    
+}
+
+consumePromiseFive();
+async function getAllUsers(params) {
+    const users = await Promise.all([promiseThree, promiseFour]);
+    console.log(users);
+}
